bd8848b73bff566ac078af25acf4b6c5f32cfb7a
marcusonline97 <67122089+marcusonline97@users.noreply.github.com>
2020-12-02 15:40:48 +0100

    added starting ai

--- Assets/Scripts/Enemies/EnemyAI.cs
+++ Assets/Scripts/Enemies/EnemyAI.cs
@@ -1,18 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class EnemyAI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    private Vector3 startingPosition;
+
+    private void Start()
     {
-        
+        startingPosition = transform.position;
+    }
+    public static Vector3 GetRandomDir
+        return new Vector3(Unityengine.Random.range(-1f,1f ),UnityEngine.Random.range(-1f,1f))normalized;
+
+    private Vector3 GetRoamingPosition()
+    {
+
+        return startingPosition + GetRandomDir() * Random.Range(10F, 70f);
     }
 }
