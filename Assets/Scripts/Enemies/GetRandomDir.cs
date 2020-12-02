227578a067adc7bcb2087a30385dc768cefbe29b
Alex Rynkowski <alex.rynkowski90@gmail.com>
2020-12-02 14:38:36 +0100

    removes GetRandomDir.cs again
    acctidentaly readded it

--- Assets/Scripts/Enemies/GetRandomDir.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-// public class GetRandomDir : MonoBehaviour
-// {
-//   public static Vector3 GetRandomDir
-//         return new Vector3(Unityengine.Random.range(-1f,1f ),UnityEngine.Random.range(-1f,1f))normalized;
-// }
