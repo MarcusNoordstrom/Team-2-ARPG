4cb28d256b2d474884acdb22569ce3602de13f61
h-Nystrom <71410719+h-Nystrom@users.noreply.github.com>
2020-12-02 11:20:08 +0100

    Added health and attack scripts

---
+++ Assets/Scripts/Unit/Attack.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace Unit{
+    public class Attack : MonoBehaviour{
+        public int attackDamage = 10; //Change to private and add a propertie
+        public string attackLayerName;
+
+        private void OnTriggerEnter(Collider other){
+            if (other.gameObject.layer == LayerMask.NameToLayer(attackLayerName)){
+                other.GetComponent<Health>().TakeDamage(attackDamage);
+            }
+        }
+    }
+}
+
