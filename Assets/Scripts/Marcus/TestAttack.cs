12445942f2bd389e7e96e8a6ce91951a5fce1696
Bulteze <48334398+Bulteze@users.noreply.github.com>
2020-12-02 17:46:15 +0100

    Added States, A Main Menu, a Pause Menu

---
+++ Assets/Scripts/Marcus/TestAttack.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using Unit;
+using UnityEngine;
+
+public class TestAttack : MonoBehaviour
+{
+    public int damage = 10; //Part of weaponSO
+    private Health targetHealth;
+    public float timer;
+    private float initTimer;
+    
+    
+    private void Awake() {
+        initTimer = timer;
+    }
+    
+    // private IEnumerator Attacking(){
+    //     while (true){
+    //         yield return new WaitForSeconds(0.1f);
+    //         if ((Time.time - timer) > speed){
+    //             targetHealth.TakeDamage(damage);//Change this!
+    //             timer = Time.time;
+    //         }
+    //     }
+    // }
+    
+    //A way to do it without Coroutine
+    private void OnCollisionEnter(Collision other) {
+        timer--;
+        if (timer <= 0 && other.gameObject.CompareTag("Player")) {
+            Debug.Log("B");
+            targetHealth = other.gameObject.GetComponent<Health>();
+            targetHealth.TakeDamage(damage);
+            timer = initTimer;
+        }
+    }
+
+    // private void OnTriggerEnter(Collider other){
+    //     if (other.gameObject.layer == LayerMask.NameToLayer("Player")){
+    //         targetHealth = other.gameObject.GetComponent<Health>();
+    //         if (attacking != null)
+    //             StopCoroutine(attacking);
+    //         attacking = Attacking();
+    //         StartCoroutine((attacking));
+    //     }
+    // }
+    // private void OnTriggerExit(Collider other){
+    //     if (other.gameObject.layer == LayerMask.NameToLayer("Player")){
+    //         StopCoroutine(attacking);
+    //         targetHealth = null;
+    //     }
+    // }
+}
--- Assets/Scripts/Unit/Health.cs
+++ Assets/Scripts/Unit/Health.cs
@@ -5,8 +5,11 @@ namespace Unit{
     public class Health : MonoBehaviour{
         private int maxHealth = 100;//ScriptableObject: setup method or awake?
         private int health;
-        public bool isDead;//Change to an event that the units component uses!
+        private bool isDead;//Change to an event that the units component uses!
         public Slider healthSlider;
+        
+        public int CurrentHealth => health;
+        public bool IsDead => isDead;
         
         public void TakeDamage(int damage){ 
             health = Mathf.Max(0, health - damage);
@@ -17,7 +20,6 @@ namespace Unit{
         }
         void Awake(){
             health = maxHealth;
-            //TODO: 
         }
     }
 }
