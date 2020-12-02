a60d0c3afa94241f3a1e328a83998dbe79efd469
Alex Rynkowski <alex.rynkowski90@gmail.com>
2020-12-02 15:09:30 +0100

    moves back rotation to update + removes RotationBody.cs and moves it into Mover.cs

--- Assets/Scripts/Player/Mover.cs
+++ Assets/Scripts/Player/Mover.cs
@@ -17,6 +17,7 @@ namespace Player {
             Movement(KeyCode.S, Vector3.back);
             Movement(KeyCode.D, Vector3.right);
             Movement(KeyCode.A, Vector3.left);
+            RotateTowardsMouse();
         }
 
         void Movement(KeyCode input, Vector3 direction) {
@@ -25,6 +26,20 @@ namespace Player {
             }
             else {
                 this._rigidbody.velocity = Vector3.zero;
+            }
+        }
+
+        void RotateTowardsMouse() {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+
+            if (Physics.Raycast(ray, out var hit)) {
+                var dir = hit.point - this.transform.position;
+                var lookRotation = Quaternion.LookRotation(dir);
+
+                Debug.DrawRay(this.transform.position, dir, Color.blue);
+                var rotation = lookRotation.eulerAngles;
+                this.transform.rotation = Quaternion.Euler(0f, rotation.y, 0f);
             }
         }
     }
