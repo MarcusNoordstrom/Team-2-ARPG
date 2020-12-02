b3895bdbcb9b82a0da106f6c4bcca008b6980803
Kevin Lempa <kevin.lempa@student.forsbergsskola.se>
2020-12-02 17:00:23 +0100

    adds EnemyAI Tracking / Patrolling behaviour

--- Assets/Scripts/Enemies/EnemyAI.cs
+++ Assets/Scripts/Enemies/EnemyAI.cs
@@ -1,23 +1,72 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class EnemyAI : MonoBehaviour
-{
+using UnityEngine.AI;
+using Random = UnityEngine.Random;
+
+public class EnemyAI : MonoBehaviour {
+    private enum State {
+        Roaming,
+        ChaseTarget,
+    }
 
     private Vector3 startingPosition;
+    private Vector3 roamPosition;
+    private NavMeshAgent pathfindingMovement;
+    public Transform target;
+    private State state;
 
-    private void Start()
-    {
+
+    private void Awake() {
+        pathfindingMovement = GetComponent<NavMeshAgent>();
+        state = State.Roaming;
+    }
+
+    private void Start() {
         startingPosition = transform.position;
+        roamPosition = GetRoamingPosition();
+        // pathfindingMovement.SetDestination(target.position);
     }
 
     public static Vector3 GetRandomDir() {
-        return new Vector3(Random.Range(-1f, 1f), UnityEngine.Random.Range(-1f, 1f)).normalized;
+        return new Vector3(Random.Range(-1f, 1f), 0, UnityEngine.Random.Range(-1f, 1f)).normalized;
     }
 
-    private Vector3 GetRoamingPosition()
-    {
+    private void Update() {
+        switch (state) {
+            default:
+            case State.Roaming:
 
-        return startingPosition + GetRandomDir() * Random.Range(10F, 70f);
+                pathfindingMovement.SetDestination(roamPosition);
+                var reachedPositionDistance = 1f;
+                if (Vector3.Distance(transform.position, roamPosition) < reachedPositionDistance) {
+                    roamPosition = GetRoamingPosition();
+                }
+
+                FindTarget();
+                break;
+            case State.ChaseTarget:
+
+                pathfindingMovement.SetDestination(target.position);
+                var attackRange = 5f;
+                if (Vector3.Distance(transform.position, target.position) < attackRange) {
+                    pathfindingMovement.isStopped = true;
+                } else pathfindingMovement.isStopped = false;
+
+
+                break;
+        }
+    }
+
+    private Vector3 GetRoamingPosition() {
+        return startingPosition + GetRandomDir() * Random.Range(3f, 10f);
+    }
+
+    private void FindTarget() {
+        float targetRange = 7f;
+        if (Vector3.Distance(transform.position, target.position) < targetRange) {
+            state = State.ChaseTarget;
+        }
     }
 }
