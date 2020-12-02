0d4086c775fa9ac8ef744c86bae1395b2d0805d0
h-Nystrom <71410719+h-Nystrom@users.noreply.github.com>
2020-12-02 17:02:04 +0100

    Update to health damageUI and attack

---
+++ Assets/Scripts/Unit/DamageUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEditor;
+using UnityEngine.UI;
+using UnityEngine;
+
+namespace Unit{
+    public class DamageUI : MonoBehaviour{
+        //TODO: move the text
+        public Text textUI;
+        private Vector3 moveUpSpeed;
+        public float speed;
+        
+        public void SetUp(int damage){
+            textUI.text = $"-{damage}";
+        }
+
+        private void Start(){
+            Destroy(this.gameObject, 2f);
+            moveUpSpeed.y = speed;
+        }
+
+        private void FixedUpdate(){
+            transform.position += moveUpSpeed * Time.deltaTime;
+            //textUI.color.a -= * Time.deltaTime;
+        }
+        
+    }
+}
+
