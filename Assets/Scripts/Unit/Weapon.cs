53a42c7dd3c0d39954e20560e506a2ae79cf6617
Kevin Lempa <kevin.lempa@student.forsbergsskola.se>
2020-12-04 14:06:14 +0100

    update Weapon.cs

--- Assets/Scripts/Unit/Weapon.cs
+++ Assets/Scripts/Unit/Weapon.cs
@@ -1,11 +1,17 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Unit {
     [CreateAssetMenu]
     public class Weapon : ScriptableObject {
         public WeaponType weaponType;
         public int baseDamage;
+        public float attackSpeed;
+        public int range;
+        public GameObject weaponPrefab;
+        
     }
+    
 
     public enum WeaponType {
         Melee,
