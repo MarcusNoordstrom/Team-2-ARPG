e379a473b4b9d5e09d9dddec883e63f59ab2b675
Bulteze <48334398+Bulteze@users.noreply.github.com>
2020-12-08 10:39:46 +0100

    WIP

--- Assets/Scripts/Unit/LookAtTarget.cs
+++ Assets/Scripts/Unit/LookAtTarget.cs
@@ -3,14 +3,19 @@
 namespace Unit {
     public class LookAtTarget : MonoBehaviour {
         Transform target;
-
+        [SerializeField] Transform partToRotate;
+        
+        
         void Awake() {
             enabled = false;
+            if (partToRotate == null) {
+                partToRotate = this.transform;
+            }
         }
 
         void FixedUpdate() {
-            var lookRotation = Quaternion.LookRotation((target.position - transform.position).normalized);
-            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, 10f * Time.deltaTime);
+            var lookRotation = Quaternion.LookRotation((target.position - partToRotate.position).normalized);
+            partToRotate.rotation = Quaternion.Slerp(partToRotate.rotation, lookRotation, 10f * Time.deltaTime);
         }
 
         public void Setup(Transform target) {
