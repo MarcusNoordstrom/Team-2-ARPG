c9f784dfe28ce76f7034078f8b4890e67dbd431f
Bulteze <48334398+Bulteze@users.noreply.github.com>
2020-12-08 16:52:39 +0100

    Added Checkpoint Respawn mechanics

---
+++ Assets/Checkpoint.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Checkpoint : MonoBehaviour {
+    public static Transform CheckpointTransform;
+    [SerializeField] Transform spawnPoint;
+    
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player")) {
+            CheckpointTransform = spawnPoint;
+        }
+    }
+}
