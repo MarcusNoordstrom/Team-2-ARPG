939a512ba53cf0436e9c9dbc14383bbbe9b0d2cd
Bulteze <48334398+Bulteze@users.noreply.github.com>
2020-12-10 12:11:44 +0100

    Updated Main Menu to Work

--- Assets/Scripts/MainMenu/MainMenuHandler.cs
+++ Assets/Scripts/MainMenu/MainMenuHandler.cs
@@ -6,13 +6,11 @@ using UnityEngine.UI;
 namespace MainMenu {
     public class MainMenuHandler : MonoBehaviour {
         public GameObject mainMenu;
-        public GameObject settingsMenu;
         public Text playBtnTxt;
         public Camera mainMenuCamera;
         public AudioListener mainMenuAudioListener => mainMenuCamera.GetComponent<AudioListener>();
 
         void Awake() {
-            settingsMenu.SetActive(false);
             playBtnTxt.text = SceneManager.sceneCount <= 1 ? "Play" : "Resume";
             if (playBtnTxt.text != "Resume") return;
             Destroy(mainMenuAudioListener);
@@ -21,12 +19,6 @@ namespace MainMenu {
 
         public void ShowMainMenu() {
             mainMenu.SetActive(true);
-            settingsMenu.SetActive(false);
-        }
-
-        public void ShowSettingsMenu() {
-            mainMenu.SetActive(false);
-            settingsMenu.SetActive(true);
         }
 
         public void Play() {
