418606afebc3418100aec9a6213f7f726e155b61
Alex Rynkowski <alex.rynkowski90@gmail.com>
2020-12-13 20:30:24 +0100

    fixes UI camera. Set the tranform to main cameras transform

--- Assets/Scripts/MainCamera/CameraFollow.cs
+++ Assets/Scripts/MainCamera/CameraFollow.cs
@@ -2,13 +2,16 @@
 
 namespace MainCamera {
     public class CameraFollow : MonoBehaviour {
+        [SerializeField] Camera uiCamera;
         Transform player;
 
         void Start() {
             player = FindObjectOfType<Player.PlayerController>().transform;
+            uiCamera.transform.position = Camera.main.transform.position;
+            uiCamera.transform.rotation = Camera.main.transform.rotation;
         }
 
-        void LateUpdate(){
+        void LateUpdate() {
             transform.position = player.position;
         }
     }
