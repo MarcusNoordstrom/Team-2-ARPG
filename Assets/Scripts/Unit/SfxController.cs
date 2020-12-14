b4a16fa248e3708d0c6071457b8a7f10c7c313cd
h-Nystrom <71410719+h-Nystrom@users.noreply.github.com>
2020-12-14 13:57:39 +0100

    Added choice beween 2d and 3d audio

--- Assets/Scripts/Unit/SfxController.cs
+++ Assets/Scripts/Unit/SfxController.cs
@@ -5,33 +5,38 @@ namespace Unit{
     [RequireComponent(typeof(AudioSource))]
     public class SfxController : MonoBehaviour{
         [SerializeField] UnitSfxClip[] sfxClips;
-        [SerializeField] Dictionary<UnitSfxId,AudioClip> sfxClipsDictionary = new Dictionary<UnitSfxId, AudioClip>();
+        [SerializeField] Dictionary<UnitSfxId, AudioClip> sfxClipsDictionary = new Dictionary<UnitSfxId, AudioClip>();
         AudioSource audioSource;
 
         void Awake(){
             audioSource = GetComponent<AudioSource>();
             foreach (var sfxClip in sfxClips){
-                sfxClipsDictionary.Add(sfxClip.id,sfxClip.audioClip);
+                sfxClipsDictionary.Add(sfxClip.id, sfxClip.audioClip);
             }
         }
-
         public void OnPlay(UnitSfxId id){
+            PlayAudioClip(sfxClipsDictionary[id], 1);
+        }
+        public void OnPlay2D(UnitSfxId id){
+            PlayAudioClip(sfxClipsDictionary[id], 0);
+        }
+        private void PlayAudioClip(AudioClip audioClip, float spatialBlend){
             audioSource.Stop();
-            audioSource.clip = sfxClipsDictionary[id];
+            audioSource.spatialBlend = spatialBlend;
+            audioSource.clip = audioClip;
             audioSource.Play();
         }
     }
-    
-    // [CreateAssetMenu(menuName = "Sound/Sfx/Ui")]
-    // public class UiSfxClip : ScriptableObject{
-    //     public UiSfxId sfx;
-    //     public AudioClip audioClip;
-    // }
-    //
-    // [System.Serializable]
-    // public enum UiSfxId{
-    //     Click,
-    //     Hover
-    // }
 }
 
+// [CreateAssetMenu(menuName = "Sound/Sfx/Ui")]
+// public class UiSfxClip : ScriptableObject{
+//     public UiSfxId sfx;
+//     public AudioClip audioClip;
+// }
+//
+// [System.Serializable]
+// public enum UiSfxId{
+//     Click,
+//     Hover
+// }
