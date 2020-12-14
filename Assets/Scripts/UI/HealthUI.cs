20a0fbc6ffd03b1cbfa59b9b63e9192c0a7b5276
Bulteze <48334398+Bulteze@users.noreply.github.com>
2020-12-14 14:56:00 +0100

    Updated Health UI to work.

--- Assets/Scripts/UI/HealthUI.cs
+++ Assets/Scripts/UI/HealthUI.cs
@@ -20,12 +20,9 @@ namespace UI {
 
         Health _health;
         bool _soundTriggered;
-        
 
         void Start() {
             _health = GetComponent<Health>();
-
-            //healthBarUI.GetComponent<HealthBarUI>().InstantiateHealthTicks();
             _health.UpdatePlayerHealthUI += PlayerHealthStuff;
         }
         
@@ -33,7 +30,8 @@ namespace UI {
         void PlayerHealthStuff(int damage) {
             if (LayerMask.GetMask() == LayerMask.NameToLayer("Player")) return;
             
-            healthBarUI.GetComponent<Slider>().value = _health.CurrentHealth * 10f;
+            var sliderValue = (float)_health.CurrentHealth / GetComponent<IGetMaxHealth>().MaxHealth() * 100;
+            healthBarUI.GetComponent<Slider>().value = Mathf.RoundToInt(sliderValue);
             
             // takingDamageEvent?.Invoke(CurrentHealth * 0.5f);
             if (LowHealth()) {
@@ -53,7 +51,7 @@ namespace UI {
 
         public void OnResurrect(bool onCorpse) {
             _soundTriggered = false;
-            healthBarUI.GetComponent<Slider>().value = _health.CurrentHealth * 10f;
+            healthBarUI.GetComponent<Slider>().value = _health.CurrentHealth * 0.1f;
         }
 
         public void Flashing() {
