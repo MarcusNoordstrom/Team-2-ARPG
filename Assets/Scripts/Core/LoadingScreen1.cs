4c2cafc9b2106e7a491966b6c2fd8e77586a6e3e
marcusonline97 <67122089+marcusonline97@users.noreply.github.com>
2020-12-15 22:45:56 +0100

    update to loadingscreen1
    updated the code in loadingscreen1 script

--- Assets/Scripts/Core/LoadingScreen1.cs
+++ Assets/Scripts/Core/LoadingScreen1.cs
@@ -5,8 +5,14 @@ public class LoadingScreen1 : MonoBehaviour
     public static LoadingScreen1 instance;
 
     [SerializeField]
-    public GameObject loading_Bar_Holder;   
+    private GameObject loading_Bar_Holder;
 
+    [SerializeField]
+    private GameObject loading_Bar_Process;
+
+    void Awake() {
+        MakeSingleton();
+    }
 
     // Start is called before the first frame update
     void Start() { 
