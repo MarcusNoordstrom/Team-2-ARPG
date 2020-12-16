fc4edfc888fe596f6d6aa8e89136b0c4c7386f87
h-Nystrom <71410719+h-Nystrom@users.noreply.github.com>
2020-12-16 15:50:05 +0100

    LevelUpdate

--- Assets/MusicController.cs
+++ Assets/MusicController.cs
@@ -1,18 +1,34 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using GameStates;
 using UnityEngine;
 
-public class MusicController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class MusicController : MonoBehaviour, IResurrect{
+    AudioSource audioSource => GetComponent<AudioSource>();
+    [SerializeField] float fadeSpeed;
+    float startVolume;
+    private void Start(){
+        startVolume = audioSource.volume;
+        audioSource.Play();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+   public void OnDeath(){
+       StartCoroutine("AudioFade");
+   }
+
+   IEnumerator AudioFade(){
+       while (audioSource.volume > 0){
+           audioSource.volume -= fadeSpeed * Time.deltaTime;
+           yield return new WaitForFixedUpdate();
+       }
+       audioSource.Stop();
+   }
+
+    public void OnResurrect(bool onCorpse){
+        StopCoroutine("AudioFade");
+        audioSource.volume = startVolume;
+        audioSource.Play();
         
     }
 }
