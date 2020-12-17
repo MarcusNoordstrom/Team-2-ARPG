e61581e6586eaf4aaf73d920204240843d732313
Alex Rynkowski <alex.rynkowski90@gmail.com>
2020-12-17 19:40:36 +0100

    starts adding drones to level 2

---
+++ Assets/Scripts/Unit/Enemy/RescaleEnemy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+
+//some enemies scale really weird.
+//Normal size when working in scene, but changes in game (wtf?!?!?)
+namespace Unit {
+    public class RescaleEnemy : MonoBehaviour {
+        void Start() {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
+}
